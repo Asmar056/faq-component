39695aa35f59028bb0b32c2f1d16400c5b7a170e
Asmar Ismayilova <asmar.ismayilova2@technipenergies.com>
2023-09-30 23:18:28 +0400

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'faq-component';
+  questions = [
+    {
+      question: 'How many bones does a cat have?',
+      answer: 'A cat has 230 bones - 6 more than a human',
+    },
+    {
+      question: 'How much do cats sleep?',
+      answer: 'The average cat sleeps 12-16 hours per day',
+    },
+    {
+      question: 'How long do cats live',
+      answer:
+        'Outdoor cats live 5 years on average. Indoor\ncats live 15 years on average.',
+    },
+  ];
+  panelOpenState = false;
+
+  
 }
